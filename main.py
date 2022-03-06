60e957803269e4566387bc6ade6091dbe0dbb48e
elem <herbet.hadrien@gmail.com>
2022-03-06 03:59:03 +0100

    [IMP] fix main file

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from controllers.controller import ControllerRunGame
 def main():
     """ The main finder called the Program class """
     def __init__(self):
+        import pdb; pdb.set_trace()
         ControllerRunGame.run()
 
 if __name__ == '__main__':
     main()
+
+
+main()
+
+test = ControllerRunGame()
+test.run()
