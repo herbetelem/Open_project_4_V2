a565b1e1229bd580648603bbb27550d9acb8e3e2
elem <herbet.hadrien@gmail.com>
2022-03-09 12:01:55 +0100

    [IMP] add the docstring and pylint for tournament

--- controllers/tournament.py
+++ controllers/tournament.py
@@ -1,3 +1,5 @@
+""" Module Tournament """
+
 import pygame
 
 from models.date import Date_selected as m_date
@@ -11,7 +13,6 @@ class Tournament:
 
     def __init__(self, c_main):
         self.c_main = c_main
-
 
         # all method for the create
         self.switch_tournament = {
@@ -31,18 +32,23 @@ class Tournament:
     def create_tournament(self):
         """ Method to manage the step of creation of the Tournament """
 
-        self.switch_tournament.get(self.c_main.m_game.step, self.create_tournament_name)()
+        self.switch_tournament.get(
+            self.c_main.m_game.step, self.create_tournament_name)()
 
     def create_tournament_name(self):
         """ Method for create the tournament's name """
 
-        self.c_main.v_basic.print_sentence("Please enter the name of the tournament")
-        input = self.c_main.v_basic.set_an_image('assets/button/input.png', (700, 150))
-        self.c_main.v_basic.manage_view(input, (((1280 - 700) / 2), 300))
-        text = self.c_main.m_game.font.render(self.c_main.m_tournament.name, 1, (255, 255, 255))
+        self.c_main.v_basic.print_sentence(
+            "Please enter the name of the tournament")
+        bkg_input = self.c_main.v_basic.set_an_image(
+            'assets/button/input.png', (700, 150))
+        self.c_main.v_basic.manage_view(bkg_input, (((1280 - 700) / 2), 300))
+        text = self.c_main.m_game.font.render(
+            self.c_main.m_tournament.name, 1, (255, 255, 255))
         self.c_main.v_basic.manage_view(text, (((1280 - 700) / 2 + 50), 340))
         if len(self.c_main.m_tournament.name) > 0:
-            self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
+            self.c_main.v_basic.manage_view(
+                self.c_main.m_game.next, self.c_main.m_game.next_rect)
 
     def create_tournament_place(self):
         """ Method for select the tournament's country """
@@ -50,18 +56,22 @@ class Tournament:
         if self.c_main.m_game.step == "country":
             sentence = "Please, choose the country"
             self.c_main.v_basic.print_sentence(sentence)
-            self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
+            self.c_main.v_basic.manage_view(
+                self.c_main.m_game.next, self.c_main.m_game.next_rect)
             data = self.c_main.m_sql.get_country()
         elif self.c_main.m_game.step == "town":
             sentence = "Please, choose the town"
             self.c_main.v_basic.print_sentence(sentence)
-            self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
+            self.c_main.v_basic.manage_view(
+                self.c_main.m_game.next, self.c_main.m_game.next_rect)
             data = self.c_main.m_sql.get_town(self.c_main.m_tournament.country)
         elif self.c_main.m_game.step == "location":
             sentence = "Please, choose the location"
             self.c_main.v_basic.print_sentence(sentence)
-            self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
-            data = self.c_main.m_sql.get_location(self.c_main.m_tournament.town)
+            self.c_main.v_basic.manage_view(
+                self.c_main.m_game.next, self.c_main.m_game.next_rect)
+            data = self.c_main.m_sql.get_location(
+                self.c_main.m_tournament.town)
 
         sentence_tmp = pygame.font.Font(None, 130).render(
             "<  " + str(data[self.c_main.m_game.index_location][1]) + "  >",
@@ -74,23 +84,34 @@ class Tournament:
 
         font_date = pygame.font.Font(None, 70)
         self.c_main.v_basic.print_sentence("Please choose the date")
-        self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
-        self.c_main.v_basic.print_sentence(str(self.c_main.m_tournament.day.str), font_date, self.c_main.m_tournament.day.rect)
-        self.c_main.v_basic.print_sentence(str(self.c_main.m_tournament.month.str), font_date, self.c_main.m_tournament.month.rect)
-        self.c_main.v_basic.print_sentence(str(self.c_main.m_tournament.year.str), font_date, self.c_main.m_tournament.year.rect)
+        self.c_main.v_basic.manage_view(
+            self.c_main.m_game.next, self.c_main.m_game.next_rect)
+        self.c_main.v_basic.print_sentence(str(
+            self.c_main.m_tournament.day.str), font_date, self.c_main.m_tournament.day.rect)
+        self.c_main.v_basic.print_sentence(str(
+            self.c_main.m_tournament.month.str), font_date, self.c_main.m_tournament.month.rect)
+        self.c_main.v_basic.print_sentence(str(
+            self.c_main.m_tournament.year.str), font_date, self.c_main.m_tournament.year.rect)
 
     def create_tournament_time(self):
         """ Methode for select the type of time """
 
         self.c_main.v_basic.print_sentence("Please choose the time style")
-        self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
+        self.c_main.v_basic.manage_view(
+            self.c_main.m_game.next, self.c_main.m_game.next_rect)
         self.set_round_time()
-        self.c_main.v_basic.manage_view(self.c_main.m_game.choice_A, self.c_main.m_game.choice_A_rect)
-        self.c_main.v_basic.manage_view(self.c_main.m_game.choice_B, self.c_main.m_game.choice_B_rect)
-        self.c_main.v_basic.manage_view(self.c_main.m_game.choice_C, self.c_main.m_game.choice_C_rect)
-        self.c_main.v_basic.print_sentence("bullet", self.c_main.m_game.font, (130, 310))
-        self.c_main.v_basic.print_sentence("blitz", self.c_main.m_game.font, (530, 310))
-        self.c_main.v_basic.print_sentence("speed shot", self.c_main.m_game.font, (930, 310))
+        self.c_main.v_basic.manage_view(
+            self.c_main.m_game.choice_A, self.c_main.m_game.choice_A_rect)
+        self.c_main.v_basic.manage_view(
+            self.c_main.m_game.choice_B, self.c_main.m_game.choice_B_rect)
+        self.c_main.v_basic.manage_view(
+            self.c_main.m_game.choice_C, self.c_main.m_game.choice_C_rect)
+        self.c_main.v_basic.print_sentence(
+            "bullet", self.c_main.m_game.font, (130, 310))
+        self.c_main.v_basic.print_sentence(
+            "blitz", self.c_main.m_game.font, (530, 310))
+        self.c_main.v_basic.print_sentence(
+            "speed shot", self.c_main.m_game.font, (930, 310))
 
     def set_time(self):
         """ Method for se't up the rect of time management """
@@ -104,12 +125,18 @@ class Tournament:
     def set_round_time(self):
         """ Methode for update the display of the time selector """
 
-        self.c_main.m_game.choice_A = self.c_main.v_basic.set_an_image('assets/button/round.png', (60, 60))
-        self.c_main.m_game.choice_A_rect = self.c_main.v_basic.set_an_image_rec(self.c_main.m_game.choice_A, 50, 300)
-        self.c_main.m_game.choice_B = self.c_main.v_basic.set_an_image('assets/button/round.png', (60, 60))
-        self.c_main.m_game.choice_B_rect = self.c_main.v_basic.set_an_image_rec(self.c_main.m_game.choice_B, 450, 300)
-        self.c_main.m_game.choice_C = self.c_main.v_basic.set_an_image('assets/button/round.png', (60, 60))
-        self.c_main.m_game.choice_C_rect = self.c_main.v_basic.set_an_image_rec(self.c_main.m_game.choice_C, 850, 300)
+        self.c_main.m_game.choice_A = self.c_main.v_basic.set_an_image(
+            'assets/button/round.png', (60, 60))
+        self.c_main.m_game.choice_A_rect = self.c_main.v_basic.set_an_image_rec(
+            self.c_main.m_game.choice_A, 50, 300)
+        self.c_main.m_game.choice_B = self.c_main.v_basic.set_an_image(
+            'assets/button/round.png', (60, 60))
+        self.c_main.m_game.choice_B_rect = self.c_main.v_basic.set_an_image_rec(
+            self.c_main.m_game.choice_B, 450, 300)
+        self.c_main.m_game.choice_C = self.c_main.v_basic.set_an_image(
+            'assets/button/round.png', (60, 60))
+        self.c_main.m_game.choice_C_rect = self.c_main.v_basic.set_an_image_rec(
+            self.c_main.m_game.choice_C, 850, 300)
 
         if self.c_main.m_game.choice == 1:
             self.c_main.m_game.choice_A = self.c_main.v_basic.set_an_image(
@@ -124,9 +151,11 @@ class Tournament:
     def create_tournament_description(self):
         """ Methode for write the description of the tournament """
 
-        self.c_main.v_basic.print_sentence("Please enter the tournament's description")
-        input = self.c_main.v_basic.set_an_image('assets/button/input.png', (700, 150))
-        self.c_main.v_basic.manage_view(input, (((1280 - 700) / 2), 300))
+        self.c_main.v_basic.print_sentence(
+            "Please enter the tournament's description")
+        bkg_input = self.c_main.v_basic.set_an_image(
+            'assets/button/input.png', (700, 150))
+        self.c_main.v_basic.manage_view(bkg_input, (((1280 - 700) / 2), 300))
         text_tmp = self.c_main.m_tournament.description
         text_y = 320
         while len(text_tmp) > 0:
@@ -136,7 +165,8 @@ class Tournament:
             text_tmp = text_tmp[50:]
             text_y += 30
         if len(self.c_main.m_tournament.description) > 0:
-            self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
+            self.c_main.v_basic.manage_view(
+                self.c_main.m_game.next, self.c_main.m_game.next_rect)
 
     def create_tournament_player(self):
         """ Methode for select the player of the tournament """
@@ -147,13 +177,18 @@ class Tournament:
 
         # check if all the players have been selected
         if len(self.c_main.m_game.players) == 8:
-            self.c_main.v_basic.manage_view(self.c_main.m_game.next, self.c_main.m_game.next_rect)
+            self.c_main.v_basic.manage_view(
+                self.c_main.m_game.next, self.c_main.m_game.next_rect)
         else:
-            input = self.c_main.v_basic.set_an_image('assets/button/input.png', (600, 75))
-            self.c_main.v_basic.manage_view(input, (310, 100))
-            self.c_main.v_basic.manage_view(self.c_main.m_game.search_button, self.c_main.m_game.search_button_rect)
-            self.c_main.v_basic.print_sentence(self.c_main.m_game.players_search, self.c_main.m_game.font, (330, 120))
-            self.c_main.v_basic.print_sentence("player name", self.c_main.m_game.font, (20, 120))
+            bkg_input = self.c_main.v_basic.set_an_image(
+                'assets/button/input.png', (600, 75))
+            self.c_main.v_basic.manage_view(bkg_input, (310, 100))
+            self.c_main.v_basic.manage_view(
+                self.c_main.m_game.search_button, self.c_main.m_game.search_button_rect)
+            self.c_main.v_basic.print_sentence(
+                self.c_main.m_game.players_search, self.c_main.m_game.font, (330, 120))
+            self.c_main.v_basic.print_sentence(
+                "player name", self.c_main.m_game.font, (20, 120))
             self.search_player()
 
     def search_player(self):
@@ -161,7 +196,8 @@ class Tournament:
 
         if len(self.c_main.m_game.players_search) > 0:
             # get the list of player in the bdd
-            request = self.c_main.m_sql.get_players(self.c_main.m_game.players_search)
+            request = self.c_main.m_sql.get_players(
+                self.c_main.m_game.players_search)
             index = 0
             tmp_x = 250
             tmp_y = 250
@@ -200,7 +236,8 @@ class Tournament:
                 )
         self.c_main.m_sql.create_tournament(data, self.c_main.m_game.players)
         self.c_main.m_tournament.id = self.c_main.m_sql.get_tournament_id(
-            [self.c_main.m_tournament.name, self.c_main.m_tournament.description, self.c_main.m_tournament.date])
+            [self.c_main.m_tournament.name, self.c_main.m_tournament.description, \
+                self.c_main.m_tournament.date])
         self.c_main.m_tournament.id = self.c_main.m_tournament.id[0][0]
         self.c_main.m_game.step = "next"
 
@@ -222,8 +259,10 @@ class Tournament:
                            pygame.font.Font(None, 35), (340, 350)],
                        [f"Description: {self.c_main.m_tournament.description}",
                            pygame.font.Font(None, 35), (340, 400)],
-                       [f"Players: {self.c_main.m_tournament.players}", pygame.font.Font(None, 35), (340, 450)]]
+                       [f"Players: {self.c_main.m_tournament.players}", \
+                           pygame.font.Font(None, 35), (340, 450)]]
         for resume in list_resume:
             self.c_main.v_basic.print_sentence(resume[0], resume[1], resume[2])
-        self.c_main.v_basic.manage_view(self.c_main.m_game.start, self.c_main.m_game.start_rect)
+        self.c_main.v_basic.manage_view(
+            self.c_main.m_game.start, self.c_main.m_game.start_rect)
         self.c_main.m_game.next_up = False
